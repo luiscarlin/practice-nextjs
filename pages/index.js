6bf3662eb2d1e746f6157bcaa90e344e9d3e0c71
Luis Carlin <luiscarlin@gmail.com>
2020-09-21 18:23:50 -0400

    try go reproduce issue

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,35 @@ export default function Home() {
   return (
     <div>
       <Timer2 />
+      <Timer2 />
+      <Timer2 />
+      <Timer2 />
+      <Timer2 />
+      <Timer2 />
+      <Timer2 />
+      <Timer2 />
+      <Timer2 />
+      <Timer2 />
+      <Timer2 />
+      <Timer2 />
+      <Timer2 />
+      <Timer2 />
+      <Timer2 />
+      <Timer2 />
+      <Timer2 />
+      <Timer2 />
+      <Timer2 />
+      <Timer2 />
+      <Timer2 />
+      <Timer2 />
+      <Timer2 />
+      <Timer2 />
+      <Timer2 />
+      <Timer2 />
+      <Timer2 />
+      <Timer2 />
+      <Timer2 />
+      <Timer2 />
     </div>
   );
 }
